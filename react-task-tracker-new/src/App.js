475cf14ef17feaf9e389a076101a993d9db15ddb
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-25 20:34:16 -0400

    finished task tracker

--- react-task-tracker-new/src/App.js
+++ react-task-tracker-new/src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header.js";
 import Tasks from "./components/Tasks.js";
+import AddTask from "./components/AddTask.js";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -23,6 +25,12 @@ function App() {
       reminder: false,
     },
   ]);
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   const deleteTask = (id) => {
@@ -40,10 +48,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
+      ) : ( 
         "Please add a task"
       )}
     </div>
--- react-task-tracker-new/src/components/Button.js
+++ react-task-tracker-new/src/components/Button.js
@@ -10,7 +10,7 @@ const Button = ({ color, text, onClick }) => {
   >
     {text}
   </button>
-  );
+  ); 
 };
 
 Button.defaultProps = {
