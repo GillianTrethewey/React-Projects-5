46d16dfb5ae4ef86b1c66fc1a46b08636ec5664e
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-27 16:57:44 -0400

    appointment planner

---
+++ react-appointment-planner/src/components/ContactPicker/ContactPicker.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const ContactPicker = (props) => {
+  return (
+    <select onChange={props.addContact} name='contacts' id='contacts'>
+      <option id='default' key='1' selected disabled hidden value='Please select contact'>Please select contact</option>
+      {props.contacts.map(contact => {
+        return <option key={contact.id} id={contact.id}  value={`${contact.itemOne} ${contact.itemTwo}`}>{contact.itemOne} {contact.itemTwo}</option>
+      })}
+    </select>
+  )
+};
+
